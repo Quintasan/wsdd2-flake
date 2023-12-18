4b46b42f292592f461eb11f50aff20c40a12bac5
Michał Zając <michal.zajac@gmail.com>
2023-12-18 07:00:50 +0100

    Simplify

--- flake.nix
+++ flake.nix
@@ -13,35 +13,20 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system:
       let
-        inherit (nixpkgs) lib;
-	pkgs = nixpkgs.legacyPackages.${system};
-        packageName = "wsdd2";
+        pkgs = import nixpkgs { inherit system; };
+	packageName = "wsdd2";
       in {
         packages.${packageName} = pkgs.stdenv.mkDerivation {
-          name = "wsdd2";
-          src = pkgs.fetchFromGitHub {
-            owner = "Netgear";
-            repo = "wsdd2";
-            rev = "master";
-            hash = "sha256-K0pGqcWzTgJGdQOIvrkXUPosgaOSL1lm81MEevK8YXk=";
+            name = "wsdd2";
+            src = pkgs.fetchFromGitHub {
+              owner = "Netgear";
+              repo = "wsdd2";
+              rev = "master";
+              hash = "sha256-K0pGqcWzTgJGdQOIvrkXUPosgaOSL1lm81MEevK8YXk=";
+            };
+          
+            CFLAGS = "-O2";
+            installFlags = [ "PREFIX=${placeholder "out"}" ];
           };
-        
-          CFLAGS = "-O2";
-          installFlags = [ "PREFIX=${placeholder "out"}" ];
-        
-          meta = with pkgs.lib; {
-            description = "WSD/LLMNR Discovery/Name Service Daemon";
-            longDescription = ''
-            	The primary purpose of this project is to enable WSD on Samba servers so that network shares hosted on a Unix box can appear in Windows File Explorer / Network.
-            '';
-            homepage = "https://github.com/Netgear/wsdd2";
-            license = licenses.gpl3Plus;
-            mainprogram = "wsdd2";
-            platforms = platforms.linux;
-          };
-        };
-
-        defaultPackage = self.packages.${system}.${packageName};
-      }
-    );
+      });
 }
